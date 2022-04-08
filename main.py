b50fcd7e976e34d1251aba0b0d20c9b7d85e78ae
fcho9876 <73094975+fcho9876@users.noreply.github.com>
2022-04-08 17:16:42 +1200

    added code
    added relevant code to main.py to be able to execute other .py scripts

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+import sys
+from PyQt5.QtWidgets import QApplication
 
+from scripts.myGUI import myGUI
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = myGUI()
+    sys.exit(app.exec_())    
