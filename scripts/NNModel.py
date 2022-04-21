5d15974faeb185bd497c75c8916f274cb089aade
fcho9876 <73094975+fcho9876@users.noreply.github.com>
2022-04-21 22:36:38 +1200

    Added TODO

---
+++ scripts/NNModel.py
@@ -0,0 +1,13 @@
+from numpy import TooHardError
+from torch import nn, optim, cuda
+from torch.utils import data
+from torchvision import datasets, transforms
+import torch.nn
+import torch.nn.functional as F
+
+class NNModel(): None
+    # TODO Add training settings
+    # TODO Download training data
+    # TODO Download testing data
+
+
