595a0c0c1b966c4813e6c7512a10bc6b1c2b5ec6
fcho9876 <73094975+fcho9876@users.noreply.github.com>
2022-04-21 23:29:34 +1200

    Added download train/test data
    Added functions to download the training and testing data
    
    Added more TODOs

--- scripts/NNModel.py
+++ scripts/NNModel.py
@@ -5,9 +5,41 @@ from torchvision import datasets, transforms
 import torch.nn
 import torch.nn.functional as F
 
-class NNModel(): None
+class NNModel():
+
     # TODO Add training settings
+    def __init__(self):
+        self.batch_size = 128
+        self.device = 'cuda' if cuda.is_available() else 'cpu'
+
     # TODO Download training data
+    def downloadTrainingData(self):
+        self.train_dataset = datasets.MNIST(root = 'MNIST_Data_Train/', 
+                                            train = True, 
+                                            transform = transforms.ToTensor(), 
+                                            download = True)
     # TODO Download testing data
+    def downloadTestData(self):
+        self.test_dataset = datasets.MNIST(root = 'MNIST_Data_Test/', 
+                                            train = False, 
+                                            transform = transforms.ToTensor())
+
+    # TODO Data loader
+    def dataLoader(self):
+        self.train_loader = data.DataLoader(dataset = self.train_dataset, 
+                                            batch_size = self.batch_size, 
+                                            shuffle = True)
+
+        self.test_loader = data.DataLoader(dataset = self.train_dataset, 
+                                            batch_size = self.batch_size, 
+                                            shuffle = False)
+
+    # TODO Define first model
+
+    # TODO Define second model
+
+    # TODO Train model 
+
+    # TODO Test model
 
 
